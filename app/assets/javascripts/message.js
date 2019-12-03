7e95d6ff610055bba2d23a0deaa59f26dc1d2581
Hanamoto-masa <hanamoto.masahiko0113@gmail.com>
2019-12-03 16:12:08 +0900

    autosave

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -37,10 +37,64 @@ $(function(){
       $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight});
       $('form')[0].reset();
       $('.form__submit').prop('disabled', false);
-      // $(".form__submit").removeAttr("disabled");
     })
     .fail(function(message){
       alert("メッセージ送信に失敗しました");
     })
   })
+  var buildMessageHTML = function(message) {
+    var image = ""
+    message.image.url ? image = `<img src="${message.image.url}">` : image = ""
+    var html = `<div class="message" data-message-id=${message.id}>
+                  <div class="message__upper-info">
+                    <div class="message__upper-info__user">
+                      ${message.user_name}
+                    </div>
+                    <div class="message__upper-info__date">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="message__text">
+                    ${message.content}
+                    <div class="message__text">
+                      ${image}
+                    </div>
+                  </div>
+                </div>`
+    return html;
+  };
+  var reloadMessages = function() {
+    last_message_id = $($(".view").children()[$(".view").children().length - 1]).attr("data-message-id");
+    var httpId = location.href.match(new RegExp(/groups\/(\d+)/))[1]
+    var http = `/groups/${httpId}/api/messages`
+    $.ajax({
+      url: `/groups/${httpId}/api/messages`,
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      var insertHTML = '';
+      var pageCount = 0;
+      messages.forEach(function(newMessage){
+        insertHTML += buildMessageHTML(newMessage);
+        pageCount = 1;
+      });
+      $(".view").append(insertHTML)
+      if (pageCount === 1){
+        $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight});
+        pageCount = 0;
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  function limitedPageLoad(){
+    var url = location.href ;
+    if(url.match(/groups\/\d+\/messages/)) {
+      reloadMessages();
+    }
+  }
+  setInterval(limitedPageLoad, 7000);
 })
